647a3c32702d82e7a273f29dbf38f3ae1d7107eb
Dylan Wang <wyc6990906@hotmail.com>
2022-06-15 21:02:35 -0600

    Create a Helper Functions Module

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 //hash password
 const bcrypt = require('bcryptjs');
 const app = express();
+const {getUser,generateRandomString,urlsForUser} = require('./helper/function')
 // Basic settings
 //ejs
 app.set("view engine", "ejs");
@@ -44,39 +45,6 @@ const users = {
     password: bcrypt.hashSync("dishwasher-funk", 10)
   }
 }
-// GENERATES RANDOM STRINGS FOR SHORTURL
-const generateRandomString = function (database) {
-  let randomNumberArray = [];
-  do {
-    for (let i = 0; i < 6; i++) {
-      randomNumberArray.push(Math.floor(Math.random() * 62));
-    }
-  } while (database[randomNumberArray.join('')] !== undefined);
-  return randomNumberArray.map((item) => {
-    if (item >= 0 && item <= 9) {
-      return String.fromCharCode(item + 48); //0-9 gives numbers 0-9
-    } else if (item >= 10 && item <= 35) {
-      return String.fromCharCode(item + 55); //10-35 gives A-Z
-    } else { // 36 - 61 gives a-z
-      return String.fromCharCode(item + 61);
-    }
-  }).join('');
-};
-// getUser
-const getUser = function (value, userDB) {
-  return Object.values(userDB).find(user => user.id === value || user.email === value);
-};
-//get urlsForUser
-const urlsForUser = function (id, urlDatabase) {
-  let urls = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key].userID === id) {
-      urls[key] = urlDatabase[key];
-    }
-  }
-  return urls;
-};
-
 
 /* GET REQUESTS*/
 //just demo useless
---
+++ helper/function.js
@@ -0,0 +1,35 @@
+// GENERATES RANDOM STRINGS FOR SHORTURL
+const generateRandomString = function (database) {
+  let randomNumberArray = [];
+  do {
+    for (let i = 0; i < 6; i++) {
+      randomNumberArray.push(Math.floor(Math.random() * 62));
+    }
+  } while (database[randomNumberArray.join('')] !== undefined);
+  return randomNumberArray.map((item) => {
+    if (item >= 0 && item <= 9) {
+      return String.fromCharCode(item + 48); //0-9 gives numbers 0-9
+    } else if (item >= 10 && item <= 35) {
+      return String.fromCharCode(item + 55); //10-35 gives A-Z
+    } else { // 36 - 61 gives a-z
+      return String.fromCharCode(item + 61);
+    }
+  }).join('');
+};
+// getUser
+const getUser = function (value, userDB) {
+  return Object.values(userDB).find(user => user.id === value || user.email === value);
+};
+//get urlsForUser
+const urlsForUser = function (id, urlDatabase) {
+  let urls = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      urls[key] = urlDatabase[key];
+    }
+  }
+  return urls;
+};
+
+
+module.exports = {generateRandomString,getUser,urlsForUser}
